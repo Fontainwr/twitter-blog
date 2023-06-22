a97afd02fb033f47bd5d49cfe4d4c5c7d05426e2
fontain richardson <fontainwr@gmail.com>
2023-06-22 21:04:46 +0900

    added gem file

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_22_114208) do
+  create_table "tweets", force: :cascade do |t|
+    t.text "tweeet"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
